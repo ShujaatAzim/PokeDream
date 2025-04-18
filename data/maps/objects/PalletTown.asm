5a9460f55296ed46ba6a51832730b667d3eb117f
Shujaat Azim <shujaat.azim@gmail.com>
2025-04-18 15:44:09 +0900

    Widen route 1 and pallet town, add offsets to link maps, fix route 21 connections

--- data/maps/objects/PalletTown.asm
+++ data/maps/objects/PalletTown.asm
@@ -8,22 +8,22 @@ PalletTown_Object:
 	db $b ; border block
 
 	def_warp_events
-	warp_event  5,  5, REDS_HOUSE_1F, 1
-	warp_event 13,  5, BLUES_HOUSE, 1
-	warp_event 12, 11, OAKS_LAB, 2
-	warp_event 19,  5, PALLETTOWN_SMALL_HOUSE_1, 1
-	warp_event 25,  3, PALLET_RANCH_GATE, 3
+	warp_event  9,  5, REDS_HOUSE_1F, 1
+	warp_event 17,  5, BLUES_HOUSE, 1
+	warp_event 16, 11, OAKS_LAB, 2
+	warp_event 23,  5, PALLETTOWN_SMALL_HOUSE_1, 1
+	warp_event 29,  3, PALLET_RANCH_GATE, 3
 
 	def_bg_events
-	bg_event 13, 13, TEXT_PALLETTOWN_OAKSLAB_SIGN
-	bg_event  7,  9, TEXT_PALLETTOWN_SIGN
-	bg_event  3,  5, TEXT_PALLETTOWN_PLAYERSHOUSE_SIGN
-	bg_event 11,  5, TEXT_PALLETTOWN_RIVALSHOUSE_SIGN
+	bg_event 17, 13, TEXT_PALLETTOWN_OAKSLAB_SIGN
+	bg_event 11,  9, TEXT_PALLETTOWN_SIGN
+	bg_event  7,  5, TEXT_PALLETTOWN_PLAYERSHOUSE_SIGN
+	bg_event 15,  5, TEXT_PALLETTOWN_RIVALSHOUSE_SIGN
 
 	def_object_events
-	object_event  8,  5, SPRITE_OAK, STAY, NONE, TEXT_PALLETTOWN_OAK
-	object_event  3,  8, SPRITE_GIRL, WALK, ANY_DIR, TEXT_PALLETTOWN_GIRL
-	object_event 18, 10, SPRITE_GUARD, STAY, DOWN, TEXT_PALLETTOWN_GUARD
-	object_event 11, 14, SPRITE_FISHER, WALK, ANY_DIR, TEXT_PALLETTOWN_FISHER
+	object_event 12,  5, SPRITE_OAK, STAY, NONE, TEXT_PALLETTOWN_OAK
+	object_event  7,  8, SPRITE_GIRL, WALK, ANY_DIR, TEXT_PALLETTOWN_GIRL
+	object_event 22, 10, SPRITE_GUARD, STAY, DOWN, TEXT_PALLETTOWN_GUARD
+	object_event 15, 14, SPRITE_FISHER, WALK, ANY_DIR, TEXT_PALLETTOWN_FISHER
 
 	def_warps_to PALLET_TOWN
--- data/maps/objects/Route1.asm
+++ data/maps/objects/Route1.asm
@@ -8,13 +8,10 @@ Route1_Object:
 	def_warp_events
 
 	def_bg_events
-	bg_event  9, 27, TEXT_ROUTE1_SIGN
+	bg_event 17, 27, TEXT_ROUTE1_SIGN
 
 	def_object_events
-	object_event  5, 24, SPRITE_YOUNGSTER, WALK, UP_DOWN, TEXT_ROUTE1_YOUNGSTER1
-	object_event 15, 13, SPRITE_YOUNGSTER, WALK, LEFT_RIGHT, TEXT_ROUTE1_YOUNGSTER2
+	object_event 13, 24, SPRITE_YOUNGSTER, WALK, UP_DOWN, TEXT_ROUTE1_YOUNGSTER1
+	object_event 23, 13, SPRITE_YOUNGSTER, WALK, LEFT_RIGHT, TEXT_ROUTE1_YOUNGSTER2
 
 	def_warps_to ROUTE_1
-
-	; unused
-	warp_to 2, 7, 4
