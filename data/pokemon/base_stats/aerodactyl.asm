ddbf698274a29028fb34d96961a4ef635bc79d70
Shujaat Azim <shujaat.azim@gmail.com>
2025-06-17 00:00:00 -0400

    Add or remove rock throw for pokemon starting with A

--- data/pokemon/base_stats/aerodactyl.asm
+++ data/pokemon/base_stats/aerodactyl.asm
@@ -16,8 +16,8 @@
 	; tm/hm learnset
 	tmhm RAZOR_WIND,   WHIRLWIND,    TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  \
 	     HYPER_BEAM,   RAGE,         DRAGON_RAGE,  MIMIC,        DOUBLE_TEAM,  \
-	     REFLECT,      BIDE,         FIRE_BLAST,   SWIFT,        SKY_ATTACK,   \
-	     REST,         SUBSTITUTE,   FLY
+	     REFLECT,      ROCK_THROW,   FIRE_BLAST,   SWIFT,        SKY_ATTACK,   \
+	     REST,         ROCK_SLIDE,	 SUBSTITUTE,   FLY
 	; end
 
 	db BANK(AerodactylPicFront)
