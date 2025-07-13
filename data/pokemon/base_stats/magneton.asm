719ad27fcf3da7657ff4715409adcb283274f2a3
Shujaat Azim <shujaat.azim@gmail.com>
2025-07-13 00:00:00 -0400

    Add or remove rock throw for pokemon starting with M part 1

--- data/pokemon/base_stats/magneton.asm
+++ data/pokemon/base_stats/magneton.asm
@@ -16,8 +16,8 @@
 	; tm/hm learnset
 	tmhm TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  HYPER_BEAM,   RAGE,         \
 	     THUNDERBOLT,  THUNDER,      TELEPORT,     MIMIC,        DOUBLE_TEAM,  \
-	     REFLECT,      BIDE,         SWIFT,        REST,         THUNDER_WAVE, \
-	     SUBSTITUTE,   FLASH
+	     REFLECT,      SWIFT,        REST,         THUNDER_WAVE, SUBSTITUTE,	 \
+			 FLASH
 	; end
 
 	db BANK(MagnetonPicFront)
