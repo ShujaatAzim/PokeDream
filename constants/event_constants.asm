de10df4c81299c1ce2709775417faa42327c361e
Shujaat Azim <shujaat.azim@gmail.com>
2025-08-13 00:00:00 -0400

    Prepare for new trainers in expanded route 1 cave

--- constants/event_constants.asm
+++ constants/event_constants.asm
@@ -235,8 +235,10 @@
 	const_next $3D0
 	const_skip 3
 	const EVENT_BEAT_ROUTE_1_CAVE_TRAINER_0
+	const EVENT_BEAT_ROUTE_1_CAVE_TRAINER_1
+	const EVENT_BEAT_ROUTE_1_CAVE_TRAINER_2
 	const EVENT_GOT_HOUSEKEYS
-	const_skip 3
+	const_skip 1
 
 ; Route 2 events
 	const_next $3D8
