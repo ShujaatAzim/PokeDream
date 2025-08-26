e46d728f10cb139f9c1ddaaf0b9f251800fcc603
Shujaat Azim <shujaat.azim@gmail.com>
2025-08-26 00:00:00 -0400

    Reduce punishment for losing from half money to 10% money

--- engine/events/black_out.asm
+++ engine/events/black_out.asm
@@ -16,24 +16,55 @@ ResetStatusAndHalveMoneyOnBlackout::
 	call HasEnoughMoney
 	jr c, .lostmoney ; never happens
 
-	; Halve the player's money.
+; Lose 10% of the player's money (money -= money/10).
+
+	; fee = money / 10
 	ld a, [wPlayerMoney]
 	ldh [hMoney], a
 	ld a, [wPlayerMoney + 1]
 	ldh [hMoney + 1], a
 	ld a, [wPlayerMoney + 2]
 	ldh [hMoney + 2], a
+
+	; divisor = 10 in BCD (0x10)
 	xor a
 	ldh [hDivideBCDDivisor], a
 	ldh [hDivideBCDDivisor + 1], a
-	ld a, 2
+	ld a, $10
 	ldh [hDivideBCDDivisor + 2], a
 	predef DivideBCDPredef3
-	ldh a, [hDivideBCDQuotient]
+	; fee is now in hDivideBCDQuotient (same location as hDivideBCDDivisor)
+
+	; reload original money (DivideBCD clobbers hMoney)
+	ld a, [wPlayerMoney]
+	ldh [hMoney], a
+	ld a, [wPlayerMoney + 1]
+	ldh [hMoney + 1], a
+	ld a, [wPlayerMoney + 2]
+	ldh [hMoney + 2], a
+
+	; money = money - fee (inline BCD subtract, 3 bytes)
+	ld de, hMoney + 2
+	ld hl, hDivideBCDQuotient + 2
+	ld c, 3
+	and a                 ; clear carry for SBC chain
+.sub10pct_loop
+	ld a, [de]
+	sbc [hl]
+	daa
+	ld [de], a
+	dec de
+	dec hl
+	dec c
+	jr nz, .sub10pct_loop
+
+
+	; store result back
+	ldh a, [hMoney]
 	ld [wPlayerMoney], a
-	ldh a, [hDivideBCDQuotient + 1]
+	ldh a, [hMoney + 1]
 	ld [wPlayerMoney + 1], a
-	ldh a, [hDivideBCDQuotient + 2]
+	ldh a, [hMoney + 2]
 	ld [wPlayerMoney + 2], a
 
 .lostmoney
