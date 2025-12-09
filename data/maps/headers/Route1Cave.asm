591d9b3bec0db11327a536af89efff854714102d
Shujaat Azim <shujaat.azim@gmail.com>
2025-12-09 00:00:00 -0500

    Finish build out of route 1 cave

---
+++ data/maps/headers/Route1Cave.asm
@@ -0,0 +1,2 @@
+map_header Route1Cave, ROUTE_1_CAVE, CAVERN, 0
+end_map_header
---
+++ data/wild/maps/Route1Cave.asm
@@ -0,0 +1,16 @@
+Route1CaveWildMons:
+  def_grass_wildmons 15 ; encounter rate
+  db 2, ZUBAT
+  db 2, ZUBAT
+  db 3, ZUBAT
+  db 3, ZUBAT
+  db 4, ZUBAT
+  db 4, ZUBAT
+  db 3, ZUBAT
+  db 4, ZUBAT
+  db 4, GEODUDE
+  db 5, ONIX
+  end_grass_wildmons
+
+  def_water_wildmons 0 ; encounter rate
+	end_water_wildmons
